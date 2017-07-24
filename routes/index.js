9a3acd16a572d146f0e414fced2d67d6c927a87f
Dheeraj Manjunath <dheerajmanju1@yahoo.com>
2017-07-24 16:12:38 -0400

    first commit for {{3y2hQrBN}}

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var config = require('../config.json');
+
+const KEY = config.key;
+const TOKEN = config.token;
+
+let trelloRegex = /{{(.*)}}/gi;
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res) {
+  let body = req.body;
+  // let 
+  if(body && body.commits){
+    body.commits.forEach(function(commit){
+      let trelloCardId = commit.message.match(trelloRegex);
+      if(trelloCardId && trelloCardId[0]){
+        trelloCardId = trelloCardId[0].replace(/{/gi, '').replace(/}/gi, '');
+      }
+      console.log('trelloCardId', trelloCardId);
+      request.post({url: `https://api.trello.com/1/cards/${trelloCardId}/attachments?key=${KEY}&token=${TOKEN}`, body: {url: commit.url}});
+    });
+  }
+  res.status(200).send();
+});
+
+module.exports = router;
